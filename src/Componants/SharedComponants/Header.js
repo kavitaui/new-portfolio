f7bd839064baad7cbcd7d71ee52bef3e83f2e214
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-19 18:03:48 +0100

    use bootstap

--- src/Componants/SharedComponants/Header.js
+++ src/Componants/SharedComponants/Header.js
@@ -3,14 +3,26 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
-       <> 
-       <Link to="/home">HOME</Link>
-       <Link to="/about-us">ABOUTUS</Link>
-       <Link to="/blog">BLOG</Link>
-       <Link to="/gallary">GALLARY</Link>
-       <Link to="/price">PRICE</Link>
-       <Link to="/service">SERVICE</Link>
-       <Link to="/testimonial">TESTIMONIAL</Link>
-     </>
+        <div className = "head-wrapper">
+            <div className = "col-12 px-2 py-2">
+                <div className = "row px-2 py-2">
+                    <div className="col-5 px-2 py-2 ">
+                
+                        VENUE BOOK 
+                </div>
+                 <div className="col-7 px-2 py-2">
+                        <ul><Link to="/home">HOME</Link>
+                            <Link to="/about-us">ABOUTUS</Link>
+                            <Link to="/blog">BLOG</Link>
+                            <Link to="/gallary">GALLARY</Link>
+                            <Link to="/price">PRICE</Link>
+                            <Link to="/service">SERVICE</Link>
+                            <Link to="/testimonial">TESTIMONIAL</Link>
+                    </ul>
+                   </div>
+                    </div>
+                </div>
+            </div>
+       
     );
 }
