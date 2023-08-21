6743622190a89c9b25f8cc35d34299588355dc26
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-21 13:18:22 +0100

    create new project

--- src/Componants/SharedComponants/Header.js
+++ src/Componants/SharedComponants/Header.js
@@ -4,21 +4,21 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <div className = "head-wrapper">
-            <div className = "col-12 px-2 py-2">
-                <div className = "row px-2 py-2">
-                    <div className="col-5 px-2 py-2 ">
+            <div className = "col-12 px-3 py-2">
+                <div className = "row  py-2">
+                    <div className="col-5  py-2  ">
                 
-                        VENUE BOOK 
+                     <span >VENUE BOOK</span>    
                 </div>
-                 <div className="col-7 px-2 py-2">
-                        <ul><Link to="/home">HOME</Link>
-                            <Link to="/about-us">ABOUTUS</Link>
-                            <Link to="/blog">BLOG</Link>
-                            <Link to="/gallary">GALLARY</Link>
-                            <Link to="/price">PRICE</Link>
-                            <Link to="/service">SERVICE</Link>
-                            <Link to="/testimonial">TESTIMONIAL</Link>
-                    </ul>
+                 <div className="col-7  py-2 ">
+                    <div className="head-wrapper "> <ul><li> <Link to="/home">HOME</Link>
+                   <Link to="/about-us">ABOUTUS</Link>
+                    <Link to="/blog">BLOG</Link>
+                        <Link to="/gallary">GALLARY</Link>
+                   <Link to="/price">PRICE</Link>
+                         <Link to="/service">SERVICE</Link>
+                     <Link to="/testimonial">TESTIMONIAL</Link>
+                </li> </ul>   </div>
                    </div>
                     </div>
                 </div>
