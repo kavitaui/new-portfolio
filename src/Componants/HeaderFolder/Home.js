2455c2759e86c59ac84bd173121975f0956a55f1
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-21 13:18:48 +0100

    create home page

--- src/Componants/HeaderFolder/Home.js
+++ src/Componants/HeaderFolder/Home.js
@@ -4,8 +4,11 @@ export default function Home (){
         <div className="col-12 px-2 py-2">
             <div className="row">
             <div className="= col-6">Get Special Offer upto 15% in Our Wedding Venue</div>
-                <div className="col-6"><span className="form">Book 
+                <div className="col-6"><span >Book 
                 A Wedding Venue</span>
+                <div>
+                    
+                </div>
 
                   </div>
                    </div> </div>
