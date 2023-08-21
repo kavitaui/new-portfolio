16b6c70fc3b2560f71ee59de5f95b3d7b973fbea
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-21 18:18:47 +0100

    amke project

--- src/Componants/HeaderFolder/Home.js
+++ src/Componants/HeaderFolder/Home.js
@@ -1,17 +1,32 @@
 import React from "react";
 export default function Home (){
     return(
-        <div className="col-12 px-2 py-2">
-            <div className="row">
-            <div className="= col-6">Get Special Offer upto 15% in Our Wedding Venue</div>
-                <div className="col-6"><span >Book 
-                A Wedding Venue</span>
-                <div>
-                    
+        <div className="col-12 ">
+            <div className="row px-5 py-5 pic">
+            <div className="= col-6 content "><h1>Get Special Offer
+                 upto 15% in Our
+                  Wedding Venue</h1></div>
+                <div className="col-6 py-4 px-5">
+                <div className="col-6"></div>
+                  <div className=" col-10 px-5 booking"> <h4> Book 
+                A Wedding Venue</h4>
+                <div className="form-group px-5 py-2">
+              <form>
+                <div className="form-row   py-2   ">
+                <div> <input type = "text" className="form-control mt-2 " placeholder="Name"/></div>
+                 <div></div> <input type = "text" className="form-control mt-2  " placeholder="Phone"/>
+                  <input type = "text" className="form-control mt-2 " placeholder="Email"/>
+                  <input type = "text" className="form-control mt-2 " placeholder="Package"/>
+                  <input type = "text" className="form-control mt-2" id="datepicker" placeholder="Avilability"/>
+                  <input type = "submit" className="custom-button mt-2  " value="CHECK AVILABILITY "/>
                 </div>
-
+                
+              </form>
+              </div>
+              </div>
+</div>
                   </div>
-                   </div> </div>
+                   </div> 
            
      
     )
--- src/Componants/SharedComponants/Footer.js
+++ src/Componants/SharedComponants/Footer.js
@@ -1,5 +1,27 @@
 export default function Footer(){
     return(
-        <div>Footer contact</div>
+        
+        
+        <div className="col-12  " >
+        <div className="row  footer px-5 py-5">
+                <div  className="col-8   ">
+           <h4> With excellent value wedding packages and fantastic offers the VenueBook is 
+            the place
+             for a memorable.</h4>  </div> 
+             <div className="col-4">
+<button>SEE OUR PACKAGES</button>
+
+             </div>
+
+          </div>
+        <div className ="footer2  py-5">
+       
+        © Copyright 2023 - wealth by AlgoMindset
+        </div>
+        </div>
+
+      
+        
+
     )
 }
