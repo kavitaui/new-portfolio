612e07db1fc071b823f7995d117c4b215efac640
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-22 18:41:02 +0100

    try to complete componants

--- src/Componants/HeaderFolder/Gallary.js
+++ src/Componants/HeaderFolder/Gallary.js
@@ -1,11 +1,38 @@
+import gallarya from "../../Asset/images/gallery-3.jpg";
+import gallaryb from "../../Asset/images/gallery-4.jpg";
+import gallaryc from "../../Asset/images/gallery-5.jpg";
+import gallaryd from "../../Asset/images/gallery-7.jpg";
+import gallarye from "../../Asset/images/gallery-1.jpg";
+import gallaryf from "../../Asset/images/gallery-8.jpg";
 
 export default function Gallary() {
 
    return (
-      <div className="col-12 px-2 py-2 images">
+      <div className="col-12 px-2 py-2 ">
+         <div className="row  images">
+            <h2>Wedding Venue Gallery</h2>
+            The Gallery provides Wedding Ceremony & Reception Venue in Texas – Houston, Beaumont, and surrounding areas.</div>
          <div className="row">
-            Wedding Venue Gallery
-            The Gallery provides Wedding Ceremony & Reception Venue in Texas – Houston, Beaumont, and surrounding areas. 
-            </div></div>
+            <div className="col-2">
+               <img src={gallarye} alt="" />
+            </div>
+            <div className="col-2">
+               <img src={gallarya} alt="" />
+            </div>
+            <div className="col-2">
+               <img src={gallaryb} alt="" />
+            </div>
+            <div className="col-2">
+               <img src={gallaryc} alt="" />
+            </div>
+            <div className="col-2">
+               <img src={gallaryd} alt="" />
+            </div>
+            <div className="col-2">
+               <img src={gallaryf} alt="" />
+            </div>
+         </div>
+      </div>
+
    )
 }
--- src/Componants/HeaderFolder/Price.js
+++ src/Componants/HeaderFolder/Price.js
@@ -1,5 +1,25 @@
-export default function Price(){
-    return(
-        <div>Price</div>
+import pricing1 from "../../Asset/images/pricing-img-1.jpg";
+import pricing2 from "../../Asset/images/pricing-img-3.jpg";
+import pricing3 from "../../Asset/images/service-11.jpg";
+export default function Price() {
+    return (
+        <div className="col-12">
+            <div className="row">Wedding Packages and Offers
+            
+            </div>
+            <div className="row">
+                <div className="col-4">
+                    <img src ={pricing1} alt =""/>
+                    Romance Package
+                    $400.00
+                </div>
+                <div className="col-4"> <img src ={pricing2} alt =""/>
+                Dream Package
+                $700.00
+                </div>
+                <div className="col-4"> <img src ={pricing3} alt =""/>Celebration Package
+                $900.00</div>
+            </div>
+            </div>
     )
 }
