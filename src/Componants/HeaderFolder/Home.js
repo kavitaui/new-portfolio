f80341356a82c88001b7ed3219a9e27df246da63
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-23 18:29:00 +0100

    form validation

--- src/Componants/HeaderFolder/Home.js
+++ src/Componants/HeaderFolder/Home.js
@@ -6,24 +6,82 @@ import 'react-calendar/dist/Calendar.css';
 
 
 export default function Home() {
+    const [sucessMsg, setSucessMsg] = useState("");
+    const [formData, setFormData] = useState({
+        Name: '',
+        Phone: '',
+        Email: '',
+        Package: '',
+        Date: '',
+
+    })
+    const [errors, setErrors] = useState({
+        Name: '',
+        Phone: '',
+        Email: '',
+        Package: '',
+        Date: '',
+
+    })
+    const handleSubmit = (e) => {
+        debugger;
+        e.preventDefault();
+        if (validateForm()) {
+            setSucessMsg("Thank you for sharing details!!, we will get back to you soon.")
+            setFormData({
+                Name: '',
+                Phone: '',
+                Email: '',
+                Package: '',
+                Date: '',
+            })
+            setTimeout(() => {
+                setSucessMsg('')
+            }, 3000);
+        }
+        else setSucessMsg("form is not valid");
+    }
+    const handleChange = (e) => {
+        debugger
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+        validateForm();
+    }
+    const validateForm = () => {
+        debugger;
+        let isValid = true;
+        const newErrors = { ...errors };
+        if (formData.Name.trim() === '') {
+            newErrors.Name = 'name is reqiured'
+            isValid = false;
+        } else {
+            newErrors.Name = '';
+        }
+        setErrors(newErrors);
+        return isValid;
+    }
 
     const [date, setDate] = useState(new Date())
     const [show, setShow] = useState(false)
 
-    const formatDate=(date)=>{
+    const formatDate = (date) => {
         var month = '' + (date.getMonth() + 1);
         var day = '' + date.getDate();
         var year = date.getFullYear();
-      
+
         if (month.length < 2) {
-          month = '0' + month;
+            month = '0' + month;
         }
         if (day.length < 2) {
-          day = '0' + day;
+            day = '0' + day;
         }
-      
+
         return [month, day, year].join('/');
-      }
+    }
 
     return (
 
@@ -39,16 +97,29 @@ export default function Home() {
                         <div className="form-group px-3 py-2">
                             <form>
                                 <div className="form-row  py-2  ">
-                                    <div> <input type="text" className="form-control mt-2 " placeholder="Name" /></div>
-                                    <div></div> <input type="text" className="form-control mt-2  " placeholder="Phone" />
-                                    <input type="text" className="form-control mt-2 " placeholder="Email" />
-                                    <input type="text" className="form-control mt-2 " placeholder="Package" />
-                                    <div class="w-100">
-                                        <input type="text"  onClick={() => setShow(!show)} className="form-control mt-2" id="datepicker" placeholder="Avilability" value={formatDate(date)}/>
-                                        {show && <Calendar onChange={setDate} value={date} />}
+                                    <div>
+                                        <input
+                                            type="text"
+                                            onChange={handleChange}
+                                            className={`form-control mt-2  ${!formData.Name.length ? 'error-field' : ''} ${formData.Name.length ? 'sucess' : ''}`}
+                                            placeholder="Name"
+                                            value={formData.Name}
+                                            name="Name"
+                                        />
+                                    </div>
+                                    <p className="popup">
+                                        {errors.Name.length > 0 ? errors.Name : null}
+                                    </p>
+                                    <div></div> <input type="text" onChange={handleChange} className="form-control mt-2  " placeholder="Phone" name="Phone" value={formData.Phone} />
+                                    <input type="text" className="form-control mt-2 " onChange={handleChange} placeholder="Email" value={formData.Email} name="Email" />
+                                    <input type="text" className="form-control mt-2 " onChange={handleChange} placeholder="Package" value={formData.Package} name="Package" />
+                                    <div className="w-100">
+                                        <input type="text" onClick={() => setShow(!show)} className="form-control mt-2" id="datepicker" placeholder="Avilability" onChange={handleChange} value={formatDate(date)} name="Avilability" />
+                                        {show && <Calendar onChange={setDate} value={ formData.date} name="Date" />}
                                         <span className="cal-icon" ><BiCalendar /></span>
                                     </div>
-                                    <input type="submit" className="custom-button mt-2  " value="CHECK AVILABILITY " />
+                                    <button type="submit" onClick={handleSubmit} className="custom-button mt-2">CHECK AVILABILITY</button>
+                                    <p className="message">{sucessMsg.length ? sucessMsg : null}</p>
                                 </div>
 
                             </form>
