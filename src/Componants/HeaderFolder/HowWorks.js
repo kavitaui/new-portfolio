e6737253b987c001675d29140c2bf36cd8ca82d8
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-08-25 21:34:10 +0100

    box

--- src/Componants/HeaderFolder/HowWorks.js
+++ src/Componants/HeaderFolder/HowWorks.js
@@ -3,19 +3,23 @@ export default function HowWorks() {
         <div className="col-12 px-2 py-2 top">
             <div className="row" px-2 py-2>
                <h2>How It Works</h2>
-                    <div className="col-3">
+                    <div className="col-3 work-block-wedding">
+                        <div className="circle"><h1>01</h1></div>
                         Search Venue
                         Views Photos,Menu,Reviews the venue that’s perfect for you.
                     </div>
                     <div className="col-3">
+                    <div className="circle"><h1>02</h1></div>  
                         Select Venue
                         Select your venue managers directly and to find more details.
                     </div>
                     <div className="col-3">
+                    <div className="circle"><h1>03</h1></div>
                         Enquire Venue
                         Send Free Enquiry and Book Online For wedding ceremony.
                     </div>
                     <div className="col-3">
+                    <div className="circle"><h1>03</h1></div>
                         Book Venue
                         Book the venue by paying a token fee online for visit & book it.
                     </div>
