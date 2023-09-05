ae6a61e936cc54573608d89d45007c0e5286ccc4
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-09-05 23:27:03 +0100

    complition the project

--- src/Componants/HeaderFolder/Price.js
+++ src/Componants/HeaderFolder/Price.js
@@ -4,21 +4,27 @@ import pricing3 from "../../Asset/images/service-11.jpg";
 export default function Price() {
     return (
         <div className="col-12">
-            <div className="row">Wedding Packages and Offers
+            <div className="row "><div><h2>Wedding Packages and Offers</h2></div>
             
             </div>
-            <div className="row">
+            <div className="row picture">
                 <div className="col-4">
                     <img src ={pricing1} alt =""/>
                     Romance Package
-                    $400.00
+                   <p> $400.00</p>
+               <h5>2 Nights in a Luxurious Bridal Suite</h5>
+          <hr/>
+             
+             Glass of Sparkling Wine to celebrate
+               Beautiful gardens for photographs
+               
                 </div>
                 <div className="col-4"> <img src ={pricing2} alt =""/>
                 Dream Package
-                $700.00
+                <p>$700.00</p>
                 </div>
-                <div className="col-4"> <img src ={pricing3} alt =""/>Celebration Package
-                $900.00</div>
+                <div className="col-4"> <img src ={pricing3} alt ="" height ="240"/>Celebration Package
+                <p >$900.00</p></div>
             </div>
             </div>
     )
